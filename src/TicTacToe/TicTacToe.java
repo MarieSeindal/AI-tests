5af400d6d1735688d4b297ecdcc8eb4a32f7299f
MarieSeindal <s185363@student.dtu.dk>
2021-09-30 22:54:45 +0200

    Game end done. Setup settigns underway

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,9 +1,14 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static boolean gameOver = false;
-
-    public static char currentplayer = 'x'; //start med spiller 1, som x
+    static boolean pvp = false; //assumed vs AI
+    static char player1;
+    static char player2;
+    public static char currentPlayer = player1; //standard start med spiller 1, som x
+    static Scanner scanner = new Scanner(System.in);
 
     // 2d Array med point
     static int[][] points ={{3,2,3},{2,4,2},{3,2,3}};
@@ -14,32 +19,87 @@ public class TicTacToe {
     public static void main(String[] args) {
 
         // 2d array with the board as it is, space = vacant, x=p1, o=p2, A=any
-        char[][] currentField ={{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+        char[][] startState ={{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+
+        initGame();
+
+        play(startState);
+
+    }
 
 
+    static void initGame(){ //Prompt the player for some settings.
+        System.out.println("Welcome to Tic Tac Toe!");
+        System.out.println("Please choose: \n 1: Player vs AI \n 2: Player vs Player");
+        String userInput = scanner.nextLine();
+
+        if(userInput.equals("1")){
+            pvp = false;
+        }
+        else if (userInput.equals("2")){
+            pvp = true;
+
+        }
+
+        //Choose sides
+        System.out.println("Player 1 choose either x or o");
+        userInput = scanner.nextLine();
+        userInput.toLowerCase();
+        if(userInput.equals("x")){
+            System.out.println("Player 1 is x. \nPlayer 2 is o\n");
+            player1 = 'x';
+            player2 = 'o';
+            currentPlayer = player1;
+        }
+        else if (userInput.equals("o")){
+            System.out.println("Player 1 is o.\nPlayer 2 is x\n");
+            player1 = 'o';
+            player2 = 'x';
+            currentPlayer = player2;
+        }
+
+        //tutorial
+        System.out.println("In order to place a piece on the board, you need to type the number corresponding to the desired location.\n The locations are:");
+        System.out.println("1,2,3\n4,5,6\n7,8,9\n");
+
+    }
+
+    static void play(char[][] startState){
+        String printPlayer;
         while (!gameOver){
 
             //Promt move or calculate move.
 
             //Make move
 
+            if(pvp){ //PvP game
+                if(currentPlayer == player1)
+                    printPlayer = "Player 1";
+                else printPlayer = "Player 2";
+
+                //Player makes move
+                System.out.println("Player " + printPlayer + " choose your move");
+                // take input
+                // Fill 2d array
+            }
+            else{ //PvAI game
+                // player or AI makes move
+            }
+
             // victory or death?
-            if (EndGame(currentField) == 0){
-                //we continue game
+            if (!(EndGame(startState) == 0)){ //if board full or victory, game-over is true.
+                gameOver = true;
             }
 
             // Switch players
-            if (currentplayer == 'x') {
-                currentplayer = 'o';
-                continue;
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
             }else
-                currentplayer ='x';
+                currentPlayer = player1;
+
+            break; //todo remove
 
         }
-
-
-
-
     }
 
 
@@ -47,18 +107,16 @@ public class TicTacToe {
 
         //check current player
         if (checkPlayerwon(currentState)){
-            System.out.println("Player" + currentplayer + " have won");
-            gameOver = true;
-            if (currentplayer == 'x')
+            System.out.println("Player" + currentPlayer + " have won");
+            if (currentPlayer == 'x')
                 return 1;
             else return 2;
         }
 
         // board full
-        if (boardFull(currentState))
-
+        if (boardFull(currentState)) {
             return 3;
-
+        }
 
         return 0;
     }
@@ -76,48 +134,48 @@ public class TicTacToe {
     }
 
     static boolean checkPlayerwon(char[][] currentState){
-        if (currentState[0][0] == currentplayer &&
-            currentState[0][1] == currentplayer &&
-            currentState[0][2] == currentplayer) { //vandret top
+        if (currentState[0][0] == currentPlayer &&
+            currentState[0][1] == currentPlayer &&
+            currentState[0][2] == currentPlayer) { //vandret top
             return true;
         }
 
-        if (currentState[1][0] == currentplayer &&
-            currentState[1][1] == currentplayer &&
-            currentState[1][2] == currentplayer) { //vandret midt
+        if (currentState[1][0] == currentPlayer &&
+            currentState[1][1] == currentPlayer &&
+            currentState[1][2] == currentPlayer) { //vandret midt
             return true;
         }
-        if (currentState[2][0] == currentplayer &&
-            currentState[2][1] == currentplayer &&
-            currentState[2][2] == currentplayer) { //vandret bund
-            return true;
-        }
-
-        if (currentState[0][0] == currentplayer &&
-            currentState[1][0] == currentplayer &&
-            currentState[2][0] == currentplayer) { //lodret venstre
+        if (currentState[2][0] == currentPlayer &&
+            currentState[2][1] == currentPlayer &&
+            currentState[2][2] == currentPlayer) { //vandret bund
             return true;
         }
 
-        if (currentState[0][1] == currentplayer &&
-            currentState[1][1] == currentplayer &&
-            currentState[2][1] == currentplayer) { //lodret midt
-            return true;
-        }
-        if (currentState[0][2] == currentplayer &&
-            currentState[1][2] == currentplayer &&
-            currentState[2][2] == currentplayer) { //lodret højre
+        if (currentState[0][0] == currentPlayer &&
+            currentState[1][0] == currentPlayer &&
+            currentState[2][0] == currentPlayer) { //lodret venstre
             return true;
         }
 
-        if (currentState[0][0] == currentplayer &&
-            currentState[1][1] == currentplayer &&
-            currentState[2][2] == currentplayer) { //skråt ned
+        if (currentState[0][1] == currentPlayer &&
+            currentState[1][1] == currentPlayer &&
+            currentState[2][1] == currentPlayer) { //lodret midt
             return true;
         }
-        if (currentState[2][0] == currentplayer &&
-            currentState[1][1] == currentplayer &&
-            currentState[0][2] == currentplayer) { //skråt op
+        if (currentState[0][2] == currentPlayer &&
+            currentState[1][2] == currentPlayer &&
+            currentState[2][2] == currentPlayer) { //lodret højre
+            return true;
+        }
+
+        if (currentState[0][0] == currentPlayer &&
+            currentState[1][1] == currentPlayer &&
+            currentState[2][2] == currentPlayer) { //skråt ned
+            return true;
+        }
+        if (currentState[2][0] == currentPlayer &&
+            currentState[1][1] == currentPlayer &&
+            currentState[0][2] == currentPlayer) { //skråt op
             return true;
         }
 
